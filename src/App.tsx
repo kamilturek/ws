1496a2907ab39aa4f5e1d1f1600d6b9ea9b87685
Kamil Turek <kamil.turek@hotmail.com>
2021-01-28 21:33:46 +0100

    Make use of Trail component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-const App = () => (
-  <p>
-    Hello.
-  </p>
-);
+import { useState } from 'react';
+
+import Trail from './Components/Trail/Trail';
+import './App.css';
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Trail open={open} onClick={() => setOpen((state) => !state)}>
+      <span>Kamil</span>
+      <span>Turek</span>
+      <span>.IT</span>
+    </Trail>
+  );
+};
 
 export default App;
